d784373f7c3d6d8895127bc346433586220f4f9d
exize1 <amio121212@gmail.com>
2022-11-06 11:35:39 +0200

    fixing-socket

--- index.js
+++ index.js
@@ -18,44 +18,40 @@ mongoose.connect( process.env.DB, {useNewUrlParser: true})
 
 app.use(cors())
 
+
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 app.use('/api', routes)
 
 const server = http.createServer(app)
 
-const io = new Server(server, {
-    cors:{
-        origin: process.env.CLIENT_SIDE,
-        methods:["GET", "POST", "PUT"],
-    },
-})
+// const io = new Server(server, {
+//     cors:{
+//         origin: process.env.CLIENT_SIDE,
+//         methods:["GET", "POST", "PUT"],
+//     },
+// })
 
-io.on("connection", (socket) => {
-    console.log(`User connected ${socket.id}`);
+// io.on("connection", (socket) => {
+//     console.log(`User connected ${socket.id}`);
 
-    socket.on("creat_room", (data) => {
-        console.log("data");
-        socket.join(data)
-        console.log(`User ${socket.id} room: ${data} created`);
-        // socket.broadcast.emit("follow_the_room", data + 1)
-    })
+//     socket.on("creat_room", (data) => {
+//         console.log("data");
+//         socket.join(data)
+//         console.log(`User ${socket.id} room: ${data} created`);
+//     })
     
 
-    // socket.on("set_player", (data) => {
-    //     socket.to(data.room).emit("sending_session", data)
-    // })
-
-    socket.on("make_the_move", (data) => {
-        socket.broadcast.emit("cach_the_move", data)
-    })
+//     socket.on("make_the_move", (data) => {
+//         socket.broadcast.emit("cach_the_move", data)
+//     })
 
     
 
-    socket.on('disconnect', () => {
-        console.log('USER DISCONNECTED');
-    })
-})
+//     socket.on('disconnect', () => {
+//         console.log('USER DISCONNECTED');
+//     })
+// })
 
 server.listen(port, () => {
     console.log(`server connected on ${port}`);
