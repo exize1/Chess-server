97ec80935ad128b9c779b208ef41967be79e8b4c
exize1 <amio121212@gmail.com>
2022-11-06 11:15:28 +0200

    fixing-socket

--- index.js
+++ index.js
@@ -18,12 +18,6 @@ mongoose.connect( process.env.DB, {useNewUrlParser: true})
 
 app.use(cors())
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Methods','*')
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Authorization,token,origin');
-    next();
-    });
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 app.use('/api', routes)
